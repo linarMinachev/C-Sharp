d26e13d2d45afa9f2d7bb0a6268d166c1cc10bfc
blindstudio <blindstudio1@gmail.com>
2022-08-04 23:50:47 +0300

    Task 3 Done

--- Lesson_1/Program.cs
+++ Lesson_1/Program.cs
@@ -6,3 +6,15 @@
 7 -> нет
 */
 
+Console.WriteLine("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+
+if (number % 2 == 0)
+    {
+        Console.WriteLine(number + " - четное число: ");
+    }
+    else
+    {
+        Console.WriteLine(number + " - Нечетное число");
+    }
