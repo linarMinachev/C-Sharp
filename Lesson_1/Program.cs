0f80543cd25e73ae358f8749d879bb11a07cb825
blindstudio <blindstudio1@gmail.com>
2022-08-05 00:02:07 +0300

    Task 4 Done

--- Lesson_1/Program.cs
+++ Lesson_1/Program.cs
@@ -4,3 +4,15 @@
 5 -> 2, 4
 8 -> 2, 4, 6, 8
 */
+
+Console.WriteLine("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int startNumber = 1;
+
+
+while(startNumber <= number)
+    {
+        if(startNumber % 2 ==0)
+        Console.Write(startNumber + ", ");
+        startNumber ++;
+    }
