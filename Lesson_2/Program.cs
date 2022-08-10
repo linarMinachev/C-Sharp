e62e6eeeb7cbbae90d9169fa4423a9219cb55bb5
blindstudio <blindstudio1@gmail.com>
2022-08-11 02:56:07 +0300

    task_2 - Условие задачи 13

--- Lesson_2/Program.cs
+++ Lesson_2/Program.cs
@@ -1,32 +1,10 @@
-﻿// // int randomNumber = GetRandomNumber(10, 99);
-// // Console.WriteLine(randomNumber);
+﻿/*
 
-// // int firstDigit = randomNumber / 10;
-// // int secondtDigit = randomNumber % 10;
+Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
 
-// // int maxDigit = Max(firstDigit, secondtDigit);
-// // Console.WriteLine(maxDigit);
+645 -> 5
+78 -> третьей цифры нет
+32679 -> 6
 
-// // int GetRandomNumber(int min, int max)
-// // {
-// //     return new Random().Next(min, max + 1);
-// // }
+*/
 
-// int Max(int a, int b)
-// {
-//     int max = 0;
-//     if(a > b)
-//     {
-//         max = a;
-//     }
-//     else{
-//         max = b;
-//     }
-//     return max;
-// }
-
-Console.WriteLine("Ведите трехзначное число: ");
-int number = Convert.ToInt32(Console.ReadLine());
-
-
-// if ()
