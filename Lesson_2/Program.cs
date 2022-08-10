3693d2c6c78ec2dc8a797cb67bcd9fe446497d0f
blindstudio <blindstudio1@gmail.com>
2022-08-10 18:38:27 +0300

    ...

--- Lesson_2/Program.cs
+++ Lesson_2/Program.cs
@@ -1,2 +1,33 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// // int randomNumber = GetRandomNumber(10, 99);
+// // Console.WriteLine(randomNumber);
+
+// // int firstDigit = randomNumber / 10;
+// // int secondtDigit = randomNumber % 10;
+
+// // int maxDigit = Max(firstDigit, secondtDigit);
+// // Console.WriteLine(maxDigit);
+
+// // int GetRandomNumber(int min, int max)
+// // {
+// //     return new Random().Next(min, max + 1);
+// // }
+
+// int Max(int a, int b)
+// {
+//     int max = 0;
+//     if(a > b)
+//     {
+//         max = a;
+//     }
+//     else{
+//         max = b;
+//     }
+//     return max;
+// }
+
+int number = Console.ReadLine("Ведите трехзначное число: ");
+
+Console.WriteLine(number);
+
+
+// if ()
