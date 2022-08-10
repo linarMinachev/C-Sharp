bf5a057d662fdc9ec901d52612bc12f62725c5e7
blindstudio <blindstudio1@gmail.com>
2022-08-10 19:02:56 +0300

    ой ой ой

---
+++ Lesson_2/Program.cs
@@ -0,0 +1,32 @@
+﻿// // int randomNumber = GetRandomNumber(10, 99);
+// // Console.WriteLine(randomNumber);
+
+// // int firstDigit = randomNumber / 10;
+// // int secondtDigit = randomNumber % 10;
+
+// // int maxDigit = Max(firstDigit, secondtDigit);
+// // Console.WriteLine(maxDigit);
+
+// // int GetRandomNumber(int min, int max)
+// // {
+// //     return new Random().Next(min, max + 1);
+// // }
+
+// int Max(int a, int b)
+// {
+//     int max = 0;
+//     if(a > b)
+//     {
+//         max = a;
+//     }
+//     else{
+//         max = b;
+//     }
+//     return max;
+// }
+
+Console.WriteLine("Ведите трехзначное число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+
+// if ()
