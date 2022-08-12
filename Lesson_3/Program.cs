fb91db382d3d65840def1b34b5c24e7e9f7dbd03
blindstudio <blindstudio1@gmail.com>
2022-08-13 00:03:07 +0300

    добавление файла программы

--- Lesson_3/Program.cs
+++ Lesson_3/Program.cs
@@ -5,3 +5,19 @@ A (3,6,8); B (2,1,-7), -> 15.84
 A (7,-5, 0); B (1,-1,9) -> 11.53
 */
 
+int x1 = ReadInt("Введите координату X первой точки: ");
+int y1 = ReadInt("Введите координату Y первой точки: ");
+int x2 = ReadInt("Введите координату X второй точки: ");
+int y2 = ReadInt("Введите координату Y второй точки: ");
+
+double length = Math.Sqrt((x2 - x1) * (x2 - x1) + (y2 - y1) * (y2 - y1));
+
+Console.WriteLine($"Длинна отрезка {length}");
+
+
+// Функция ввода сообщения
+int ReadInt(string message)
+{
+    Console.Write(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
