5ae6e4e99c7566155d2032115a464fa2e7f020c9
blindstudio <blindstudio1@gmail.com>
2022-08-13 01:17:47 +0300

    Задача 19 (Task_1) - Решена

--- Lesson_3/Program.cs
+++ Lesson_3/Program.cs
@@ -7,74 +7,22 @@
 23432 -> да
 */
 
-int number = ReadInt("Введите пятизначное число: ");
-int len = number.ToString().Length;
+Console.WriteLine("Введите число: ");
+string number = Console.ReadLine();
+int len = number.Length;
 
-if (len != 5)
+if (len == 5)
 {
-    Console.WriteLine($"Число {number} не является пятизначным!");
-    return;
-}
-// Все что выше верно - Работает код
-else
-{
-    int start = 1;
-    len = len / 2;
-    for (int i = 0; i == len; i++)
+    if (number[0] == number[4] && number[1] == number[3])
     {
-        int end = number % 10;
-        start = (start * 10);
-        if(start == end)
-        {
-            i++;
-        }
-        else
-        {
-            Console.WriteLine($"{number} - Не палиндром");
-        }
-        // toTern = "result" + "result";
         Console.WriteLine($"{number} - Палиндром");
     }
-
-// int c = 1;
-//         for (int i = 0; i == len; i--)
-//         {
-//             c = c * 10;
-//         }
-
-//         int x = (number / c) % 10;
-//         int y = 
-
+    else
+    {
+        Console.WriteLine($"{number} - НЕ палиндром");
+    }
 }
-
-
-// // Вариант 2. (через строку) - вариант с проверкой на количество символов. 
-// // ПЛЮСЫ: *Может проверять не только число*
-
-// Console.WriteLine("Введите число: ");
-// string number = Console.ReadLine();
-// int len = number.Length;
-
-// if (len == 5)
-// {
-//     if (number[0] == number[4] && number[1] == number[3])
-//     {
-//         Console.WriteLine($"{number} - Палиндром");
-//     }
-//     else
-//     {
-//         Console.WriteLine($"{number} - НЕ палиндром");
-//     }
-// }
-// else
-// {
-//     Console.WriteLine($"ОШИБКА: {number} - не является пятизначным");
-// }
-
-
-// Функция ввода сообщения
-int ReadInt(string message)
+else
 {
-    Console.Write(message);
-    return Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine($"ОШИБКА: {number} - не является пятизначным");
 }
