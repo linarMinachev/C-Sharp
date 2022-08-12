664ae327a376ef6fe3486d5cf13b02f419a8218f
blindstudio <blindstudio1@gmail.com>
2022-08-12 06:08:50 +0300

    Task_2 (Задача 13) решена! Снова тренировал функции, поэтому черех них решал. се равно тяжко с 'return'

--- Lesson_2/Program.cs
+++ Lesson_2/Program.cs
@@ -10,34 +10,35 @@
 
 int number = ReadInt("Введите число: ");
 int count = number.ToString().Length;
-
 Console.Write(MakeArray(number, count));
-
-int MakeArray(int a, int b)
-{
-    if (b > 4)
-    {
-        int result = result / 10;
-        b--;
-    }
-    else
-    {
-        while (b < 3)
-        {
-            Console.WriteLine("третьей цифры нет");
-            break;
-        }
-    }
-    return result;
-}
 
 
 // ФУНКЦИИ------------------------------------------------------------------------------------------------------
 
-// // Функция принимает сообщение для отображения в консоли, и выводит результат введенных данных пользователем. 
-// int ReadInt(string message)
-// {
-//     Console.Write(message);
-//     return Convert.ToInt32(Console.ReadLine());
-// }
+// Функция принимает сообщение для отображения в консоли, и выводит результат введенных данных пользователем.
+int ReadInt(string message)
+{
+    Console.Write(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
 
+// Функция принимает число введенное пользователем, количество символов, и выводит третью цифру числа. Если 3 цифры нет, сообщает и выводит 0.
+int MakeArray(int a, int b)
+{
+int result = 0;
+    if (b < 3)
+    {
+        Console.Write("Третьей цифры нет, держи: ");
+    }
+    else
+    {
+        int c = 1;
+        for (int i = b; i > 3; i--)
+        {
+            c = c * 10;
+        }
+
+        result = (a / c) % 10;
+    }
+return result;
+}
