23f948011f35ecc04da4ff2ac654a2ae51840522
blindstudio <blindstudio1@gmail.com>
2022-08-14 02:51:06 +0300

    Условие Задачи 2

---
+++ Extra lessons/Program.cs
@@ -0,0 +1,40 @@
+﻿/*
+    Задача 1. (branch Task_1)
+    В переменной string есть секретное сообщение, во второй есть пароль.
+    Пользователю программы даётся 3 попытки ввести пароль и увидеть секретное сообщение.
+*/
+
+string secretMessage = "Секретное сообщение";
+string password = "PassWord";
+int attempts = 3;
+
+Console.WriteLine($"Колличество попыток {attempts}");
+string checkPass = ReadInput("Введите пароль: ");
+
+
+for (int i = 1; i <= attempts; i++)
+{
+    if (checkPass == password)
+    {
+        Console.WriteLine($"Пароль верный! {secretMessage}");
+        break;
+    }
+    else if (i == attempts)
+    {
+        Console.WriteLine("Колличество попыток израсходованно");
+        break;
+    }
+    else
+    {
+        Console.WriteLine($"Пароль неверный! Осталось попыток: {attempts - i} ");
+        checkPass = ReadInput("Введите пароль: ");
+    }
+}
+
+
+// Функция ввода
+string ReadInput(string msg)
+{
+    Console.Write(msg);
+    return Console.ReadLine();
+}
