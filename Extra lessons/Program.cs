4dd2ea020c87a48eea8b706f2d37540ce4b46c5a
blindstudio <blindstudio1@gmail.com>
2022-08-14 14:24:13 +0300

    Задача 2 решена

--- Extra lessons/Program.cs
+++ Extra lessons/Program.cs
@@ -3,3 +3,19 @@
     Есть программа с бесконечным циклом.
     Когда пользователь вводит exit программа заканчивается
 */
+
+string someMsg = ReadInput("Введите кодовое слово: ");
+string exit = "exit";
+
+while (someMsg != exit)
+{
+    someMsg = ReadInput("Введите кодовое слово: ");
+}
+
+
+// Функция ввода
+string ReadInput(string msg)
+{
+    Console.Write(msg);
+    return Console.ReadLine();
+}
