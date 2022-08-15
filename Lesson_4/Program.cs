435c5e6f4f1df56493a7cd2c029ca4a6a51679b2
blindstudio <blindstudio1@gmail.com>
2022-08-16 02:55:54 +0300

    Задача 25 (task_1) - Решена

--- Lesson_4/Program.cs
+++ Lesson_4/Program.cs
@@ -5,3 +5,27 @@
 2, 4 -> 16
 */
 
+int numberA = REadInt("Введите число A: ");
+int numberB = REadInt("Введите число B: ");
+ToDegree(numberA, numberB);
+
+
+
+
+// Функция возведения в степень
+void ToDegree(int a, int b)
+{
+    int result = 1;
+    for (int i = 1; i <= b; i++)
+    {
+        result = result * a;
+    }
+    Console.WriteLine(result);
+}
+
+// Функция ввода
+int REadInt(string message)
+{
+    Console.WriteLine(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
