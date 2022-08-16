3836db98aa6008a8998d80072ac7c5808f7e4530
blindstudio <blindstudio1@gmail.com>
2022-08-16 04:25:37 +0300

    задача 2 к уроку 4 РЕШЕНА

--- Lesson_4/Program.cs
+++ Lesson_4/Program.cs
@@ -5,3 +5,39 @@
 82 -> 10
 9012 -> 12
 */
+
+int number = ReadInt("Введите число: ");
+int len = NumberLen(number);
+SumNumbers(number, len);
+
+
+// Функция ввода
+int ReadInt(string message)
+{
+    Console.Write(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+// Функция подсчета цифр в числе
+int NumberLen(int a)
+{
+    int index = 0;
+    while (a > 0)
+    {
+        a /= 10;
+        index++;
+    }
+    return index;
+}
+
+// Функция вывода суммы цифр в числе
+void SumNumbers(int n, int len)
+{
+    int sum = 0;
+    for (int i = 1; i <= len; i++)
+    {
+        sum += number % 10;
+        number /= 10;
+    }
+    Console.WriteLine(sum);
+}
