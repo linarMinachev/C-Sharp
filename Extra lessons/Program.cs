dcd6c15e6713c8457dd17cfe3710ee3fbc67b169
blindstudio <blindstudio1@gmail.com>
2022-08-16 05:47:30 +0300

    черновик 2

--- Extra lessons/Program.cs
+++ Extra lessons/Program.cs
@@ -12,6 +12,58 @@ WriteName – вывести имя после ввода пароля
 
 Help();
 
+string userName = null;
+string userPassword = null;
+
+string fromUser = ReadInput("Введите команду: ");
+
+
+while (fromUser == "exit")
+{
+
+}
+switch (fromUser)
+{
+    case "help":
+        Commands();
+        break;
+    case "setName":
+        userName = ReadInput("Введите имя: ");
+        fromUser = ReadInput("Введите команду: ");
+        break;
+    case "setPassword":
+        userPassword = ReadInput("Создайте пароль: ");
+        fromUser = ReadInput("Введите команду: ");
+        break;
+    case "writeName":
+        string checkPass = ReadInput("Введите пароль: ");
+        if (checkPass == null)
+        {
+            if (userName == null)
+            {
+                Console.WriteLine("Вы не задали имя");
+                fromUser = ReadInput("Введите команду: ");
+            }
+            else
+            {
+                Console.WriteLine(userName);
+                fromUser = ReadInput("Введите команду: ");
+            }
+        }
+        else
+        {
+            Console.WriteLine("Пароль не верный");
+            fromUser = ReadInput("Введите команду: ");
+        }
+        break;
+    case "exit":
+        // string userName = ReadInput("Введите имя: ");
+        break;
+
+
+}
+
+
 // string someMsg = ReadInput("Введите кодовое слово: ");
 // string acsessCode = "exit";
 
@@ -31,16 +83,19 @@ string ReadInput(string msg)
 // Функция: Вывод команды для отображения списка команд
 void Help()
 {
-    Console.WriteLine("для вывода списка команд, напишите 'help'");
+    Console.WriteLine("Для вывода списка команд, напишите 'help'");
 }
 
 
 // Функция: Вывод команд для работы с программой
-void Commands(string help)
+void Commands()
 {
+    Console.WriteLine();
     Console.WriteLine("SetName – Установить имя");
     Console.WriteLine("SetPassword – Установить пароль");
     Console.WriteLine("WriteName – вывести имя после ввода пароля");
     Console.WriteLine("Help – вывести список команд");
     Console.WriteLine("Exit – выход");
+    Console.WriteLine();
+    fromUser = ReadInput("Введите команду: ");
 }
