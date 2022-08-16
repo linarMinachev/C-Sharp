f45a41f7a5f2ce80bf4db99fa745a9bf83137fd6
blindstudio <blindstudio1@gmail.com>
2022-08-16 04:38:30 +0300

    Задача 3 (29) - Решена

--- Lesson_4/Program.cs
+++ Lesson_4/Program.cs
@@ -4,3 +4,20 @@
 5 -> [1, 2, 5, 7, 19]
 3 -> [6, 1, 33]
 */
+
+int lenArray = ReadInt("Введите длинну массива: ");
+
+int[] randomArray = new int[lenArray];
+for (int i = 0; i < randomArray.Length; i++)
+{
+    randomArray[i] = new Random().Next(1,9);
+    Console.Write(randomArray[i] + " ");
+}
+
+
+// Функция ввода
+int ReadInt(string message)
+{
+    Console.Write(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
