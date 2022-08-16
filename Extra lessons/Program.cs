ac4a3e92ff167115e5ab9d9b148370b6b565b29a
blindstudio <blindstudio1@gmail.com>
2022-08-16 05:13:15 +0300

    1 черновик

--- Extra lessons/Program.cs
+++ Extra lessons/Program.cs
@@ -9,3 +9,38 @@ SetPassword – Установить пароль
 Exit – выход
 WriteName – вывести имя после ввода пароля
 */
+
+Help();
+
+// string someMsg = ReadInput("Введите кодовое слово: ");
+// string acsessCode = "exit";
+
+// while (someMsg != acsessCode)
+// {
+//     someMsg = ReadInput("Введите кодовое слово: ");
+// }
+
+
+// Функция ввода
+string ReadInput(string msg)
+{
+    Console.Write(msg);
+    return Console.ReadLine();
+}
+
+// Функция: Вывод команды для отображения списка команд
+void Help()
+{
+    Console.WriteLine("для вывода списка команд, напишите 'help'");
+}
+
+
+// Функция: Вывод команд для работы с программой
+void Commands(string help)
+{
+    Console.WriteLine("SetName – Установить имя");
+    Console.WriteLine("SetPassword – Установить пароль");
+    Console.WriteLine("WriteName – вывести имя после ввода пароля");
+    Console.WriteLine("Help – вывести список команд");
+    Console.WriteLine("Exit – выход");
+}
