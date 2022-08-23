9d7526f53f96a4a2daa01627b2c430ac9769cdcb
blindstudio <blindstudio1@gmail.com>
2022-08-23 11:10:14 +0300

    task_2 DONE

--- Lesson_5/Program.cs
+++ Lesson_5/Program.cs
@@ -4,3 +4,38 @@
 [-4, -6, 89, 6] -> 0
 */
 
+
+int size = 4;
+int[] numbers = new int[size];
+FillArrayRandomNumbers(numbers);
+PrintArray(numbers);
+
+int sumNumbersEvenIndex = 0;
+
+for (int i = 1; i < numbers.Length; i+=2)
+{
+    sumNumbersEvenIndex = sumNumbersEvenIndex + numbers[i];
+}
+Console.Write(sumNumbersEvenIndex);
+
+
+// Функции
+
+// Функция заполнения массива случайными числами от -100 до 100
+void FillArrayRandomNumbers(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = new Random().Next(-100, 101);
+    }
+}
+
+// Функция вывода массива в терминал
+void PrintArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write(array[i] + " ");
+    }
+    Console.WriteLine();
+}
